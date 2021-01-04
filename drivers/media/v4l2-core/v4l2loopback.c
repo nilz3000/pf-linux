0eca885811ec5826cb7dc6bd524ac8dd5f46b5d1
Oleksandr Natalenko <oleksandr@natalenko.name>
2021-01-04 11:30:51 +0100

    v4l2loopback-5.10: confine v4l2loopback_cleanup_module
    Signed-off-by: Oleksandr Natalenko <oleksandr@natalenko.name>

--- drivers/media/v4l2-core/v4l2loopback.c
+++ drivers/media/v4l2-core/v4l2loopback.c
@@ -2863,6 +2863,7 @@ error:
 	return err;
 }
 
+#ifdef MODULE
 static void v4l2loopback_cleanup_module(void)
 {
 	MARK();
@@ -2872,6 +2873,7 @@ static void v4l2loopback_cleanup_module(void)
 	misc_deregister(&v4l2loopback_misc);
 	dprintk("module removed\n");
 }
+#endif
 
 MODULE_ALIAS_MISCDEV(MISC_DYNAMIC_MINOR);
 MODULE_ALIAS("devname:v4l2loopback");
