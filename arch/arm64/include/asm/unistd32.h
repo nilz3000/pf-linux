9b1eaabb83cca7560d1bceb8820942be0f2fa1e6
Oleksandr Natalenko <oleksandr@natalenko.name>
2021-10-04 17:00:38 +0200

    mm: wire up pmadv_ksm system call
    Signed-off-by: Oleksandr Natalenko <oleksandr@natalenko.name>

--- arch/arm64/include/asm/unistd32.h
+++ arch/arm64/include/asm/unistd32.h
@@ -901,6 +901,8 @@ __SYSCALL(__NR_landlock_create_ruleset, sys_landlock_create_ruleset)
 __SYSCALL(__NR_landlock_add_rule, sys_landlock_add_rule)
 #define __NR_landlock_restrict_self 446
 __SYSCALL(__NR_landlock_restrict_self, sys_landlock_restrict_self)
+#define __NR_pmadv_ksm 447
+__SYSCALL(__NR_pmadv_ksm, sys_pmadv_ksm)
 
 /*
  * Please add new compat syscalls above this comment and update
