fb8faf79f116ea2678a5bf611b2e9f464d71dfcd
Perry Yuan <Perry.Yuan@amd.com>
2023-01-18 15:52:03 +0800

    cpufreq: amd-pstate: implement Pstate EPP support for the AMD processors
    Add EPP driver support for AMD SoCs which support a dedicated MSR for
    CPPC.  EPP is used by the DPM controller to configure the frequency that
    a core operates at during short periods of activity.
    
    The SoC EPP targets are configured on a scale from 0 to 255 where 0
    represents maximum performance and 255 represents maximum efficiency.
    
    The amd-pstate driver exports profile string names to userspace that are
    tied to specific EPP values.
    
    The balance_performance string (0x80) provides the best balance for
    efficiency versus power on most systems, but users can choose other
    strings to meet their needs as well.
    
    $ cat /sys/devices/system/cpu/cpufreq/policy0/energy_performance_available_preferences
    default performance balance_performance balance_power power
    
    $ cat /sys/devices/system/cpu/cpufreq/policy0/energy_performance_preference
    balance_performance
    
    To enable the driver,it needs to add `amd_pstate=active` to kernel
    command line and kernel will load the active mode epp driver
    
    Acked-by: Huang Rui <ray.huang@amd.com>
    Reviewed-by: Mario Limonciello <Mario.Limonciello@amd.com>
    Tested-by: Wyes Karny <wyes.karny@amd.com>
    Signed-off-by: Perry Yuan <Perry.Yuan@amd.com>

--- include/linux/amd-pstate.h
+++ include/linux/amd-pstate.h
@@ -47,6 +47,10 @@ struct amd_aperf_mperf {
  * @prev: Last Aperf/Mperf/tsc count value read from register
  * @freq: current cpu frequency value
  * @boost_supported: check whether the Processor or SBIOS supports boost mode
+ * @epp_policy: Last saved policy used to set energy-performance preference
+ * @epp_cached: Cached CPPC energy-performance preference value
+ * @policy: Cpufreq policy value
+ * @cppc_cap1_cached Cached MSR_AMD_CPPC_CAP1 register value
  *
  * The amd_cpudata is key private data for each CPU thread in AMD P-State, and
  * represents all the attributes and goals that AMD P-State requests at runtime.
@@ -72,6 +76,12 @@ struct amd_cpudata {
 
 	u64	freq;
 	bool	boost_supported;
+
+	/* EPP feature related attributes*/
+	s16	epp_policy;
+	s16	epp_cached;
+	u32	policy;
+	u64	cppc_cap1_cached;
 };
 
 /*
@@ -91,4 +101,49 @@ static const char * const amd_pstate_mode_string[] = {
 	NULL,
 };
 
+#define AMD_CPPC_EPP_PERFORMANCE		0x00
+#define AMD_CPPC_EPP_BALANCE_PERFORMANCE	0x80
+#define AMD_CPPC_EPP_BALANCE_POWERSAVE		0xBF
+#define AMD_CPPC_EPP_POWERSAVE			0xFF
+
+/*
+ * AMD Energy Preference Performance (EPP)
+ * The EPP is used in the CCLK DPM controller to drive
+ * the frequency that a core is going to operate during
+ * short periods of activity. EPP values will be utilized for
+ * different OS profiles (balanced, performance, power savings)
+ * display strings corresponding to EPP index in the
+ * energy_perf_strings[]
+ *	index		String
+ *-------------------------------------
+ *	0		default
+ *	1		performance
+ *	2		balance_performance
+ *	3		balance_power
+ *	4		power
+ */
+enum energy_perf_value_index {
+	EPP_INDEX_DEFAULT = 0,
+	EPP_INDEX_PERFORMANCE,
+	EPP_INDEX_BALANCE_PERFORMANCE,
+	EPP_INDEX_BALANCE_POWERSAVE,
+	EPP_INDEX_POWERSAVE,
+};
+
+static const char * const energy_perf_strings[] = {
+	[EPP_INDEX_DEFAULT] = "default",
+	[EPP_INDEX_PERFORMANCE] = "performance",
+	[EPP_INDEX_BALANCE_PERFORMANCE] = "balance_performance",
+	[EPP_INDEX_BALANCE_POWERSAVE] = "balance_power",
+	[EPP_INDEX_POWERSAVE] = "power",
+	NULL
+};
+
+static unsigned int epp_values[] = {
+	[EPP_INDEX_DEFAULT] = 0,
+	[EPP_INDEX_PERFORMANCE] = AMD_CPPC_EPP_PERFORMANCE,
+	[EPP_INDEX_BALANCE_PERFORMANCE] = AMD_CPPC_EPP_BALANCE_PERFORMANCE,
+	[EPP_INDEX_BALANCE_POWERSAVE] = AMD_CPPC_EPP_BALANCE_POWERSAVE,
+	[EPP_INDEX_POWERSAVE] = AMD_CPPC_EPP_POWERSAVE,
+ };
 #endif /* _LINUX_AMD_PSTATE_H */
